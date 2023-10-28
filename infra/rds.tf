622e4ec001464d0c5d7b45fb110a1f89b6be22fd
Jonilson Sousa <jonilsonds9@gmail.com>
2023-10-27 23:53:50 -0300

    Arrumando RDS subnet

--- infra/rds.tf
+++ infra/rds.tf
@@ -40,5 +40,5 @@ resource "aws_db_instance" "rds" {
   vpc_security_group_ids = [aws_security_group.rds.id]
   publicly_accessible = true
   skip_final_snapshot = true
-  db_subnet_group_name = aws_subnet.this["pub_a"].id
+  db_subnet_group_name = aws_subnet.this["pub_b"].id
 }
