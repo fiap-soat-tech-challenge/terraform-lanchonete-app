0b4e93e388bd1af222b2abda9f89dfa55ec70b4f
Jonilson Sousa <jonilsonds9@gmail.com>
2023-10-31 10:54:56 -0300

    Adicionando envvars fix json 2

--- infra/ecs.tf
+++ infra/ecs.tf
@@ -18,7 +18,7 @@ resource "aws_ecs_task_definition" "this" {
         }
       ]
       environment = [
-        { "name": "NODE_ENV", "value": "production" },
+        { "name": "NODE_ENV", "value": "development" },
         { "name": "DB_HOST", "value": "${aws_db_instance.rds.endpoint}" },
         { "name": "DB_PORT", "value": "5432" },
         { "name": "DB_USER", "value": "${var.db_username}" },
